be94fc37d0ec488c4c19ae5b2779da50577b88e6
helsonxiao <helsonxiao@gmail.com>
2022-05-22 22:24:07 +0800

    Add env package

---
+++ configs/env.go
@@ -0,0 +1,20 @@
+package configs
+
+import "os"
+
+var JudgerWorkspace string
+var TestCaseDir string
+
+func InitEnv() {
+	JudgerWorkspace = getEnv("JUDGER_WORKSPACE", "/judger/run")
+	TestCaseDir = getEnv("TEST_CASE_DIR", "/test_case")
+}
+
+func getEnv(envName string, defaultValue string) string {
+	envValue := os.Getenv(envName)
+	if envValue == "" {
+		return defaultValue
+	} else {
+		return envValue
+	}
+}
